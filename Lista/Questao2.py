18eef70c967faea04667ce95cc918e6e9888dddc
Gomes <81239085+Gomes-Jao@users.noreply.github.com>
2022-10-25 21:44:34 -0300

    Create Questao2.py

---
+++ Lista/Questao2.py
@@ -0,0 +1,9 @@
+from time import process_time
+x = 1
+count = 0
+while process_time() < 60:
+    if x == 2 or (2 ** (x-1)) % x == 1:
+        count += 1
+        print(x)
+    x += 1
+print("Primos encontrados: {}".format(count))
